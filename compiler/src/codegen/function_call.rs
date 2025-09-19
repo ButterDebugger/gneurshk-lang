2ce61e081570367c1b299fa373ced189de5cc76d
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2025-09-19 18:20:05 -0400

    Supported multiple parameters for println

--- compiler/src/codegen/function_call.rs
+++ compiler/src/codegen/function_call.rs
@@ -1,6 +1,6 @@
 use crate::codegen::Codegen;
 use gneurshk_parser::Stmt;
-use inkwell::values::BasicValueEnum;
+use inkwell::values::{BasicMetadataValueEnum, BasicValueEnum};
 
 impl<'ctx> Codegen<'ctx> {
     pub(crate) fn compile_function_call(
@@ -29,35 +29,47 @@ impl<'ctx> Codegen<'ctx> {
             .builder
             .build_call(function, &arg_values, &format!("call_{}", name))
             .unwrap();
+
         call_result.try_as_basic_value().left()
     }
 
     fn compile_println(&mut self, args: Vec<Stmt>) -> Option<BasicValueEnum<'ctx>> {
-        // Expect exactly one argument TODO: Support multiple arguments
-        if args.len() != 1 {
-            return None;
+        // Compile the arguments
+        let mut arg_values: Vec<BasicMetadataValueEnum<'ctx>> = Vec::new();
+        for arg in args {
+            if let Some(value) = self.compile_stmt(arg) {
+                arg_values.push(value.into());
+            }
         }
 
-        // Compile the argument
-        let arg_value = self.compile_stmt(args.into_iter().next().unwrap())?;
-
         // Create format string
-        let format_str = "%d\n\0";
+        let mut format_str = String::new();
+
+        for i in 0..arg_values.len() {
+            format_str.push_str("%d");
+
+            if i != arg_values.len() - 1 {
+                format_str.push(' ');
+            }
+        }
+
+        format_str.push_str("\n\0");
+
+        // Create global string
         let format_str_global = self
             .builder
-            .build_global_string_ptr(format_str, "format_str")
+            .build_global_string_ptr(&format_str, "format_str")
             .unwrap();
 
         // Get printf function
         let printf_fn = self.scope.get_function("printf")?;
 
         // Call printf
-        let args = vec![
-            format_str_global.as_pointer_value().into(),
-            arg_value.into(),
-        ];
+        let mut printf_args = vec![format_str_global.as_pointer_value().into()];
+        printf_args.extend(arg_values);
+
         self.builder
-            .build_call(printf_fn, &args, "printf_call")
+            .build_call(printf_fn, &printf_args, "printf_call")
             .unwrap();
 
         None
