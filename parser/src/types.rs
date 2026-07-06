5b3d19535eac5dd5c055a860aeb045d787112433
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2026-07-06 19:36:03 -0400

    Updated data types
    - Added more
    - Updated parsing and displaying them

--- parser/src/types.rs
+++ parser/src/types.rs
@@ -1,37 +1,47 @@
 use anyhow::{Result, anyhow};
 use gneurshk_lexer::{TokenStream, tokens::Token};
-use std::fmt::Display;
+use std::str::FromStr;
+use strum_macros::EnumString;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, strum_macros::Display, EnumString)]
 pub enum DataType {
+    // Integer types
+    #[strum(to_string = "Int8")]
+    Int8,
+    #[strum(to_string = "Int16")]
+    Int16,
+    #[strum(to_string = "Int32")]
     Int32,
+    #[strum(to_string = "Int64")]
+    Int64,
+    // Unsigned integer types
+    #[strum(to_string = "UInt8")]
+    UInt8,
+    #[strum(to_string = "UInt16")]
+    UInt16,
+    #[strum(to_string = "UInt32")]
+    UInt32,
+    #[strum(to_string = "UInt64")]
+    UInt64,
+    // Floating point types
+    #[strum(to_string = "Float32")]
     Float32,
+    #[strum(to_string = "Float64")]
+    Float64,
+    // Other types
+    #[strum(to_string = "String")]
     String,
+    #[strum(to_string = "Boolean")]
     Boolean,
+    #[strum(to_string = "{0}")]
     Custom(String),
-}
-
-impl Display for DataType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DataType::Int32 => write!(f, "Int32"),
-            DataType::Float32 => write!(f, "Float32"),
-            DataType::String => write!(f, "String"),
-            DataType::Boolean => write!(f, "Boolean"),
-            DataType::Custom(name) => write!(f, "{}", name),
-        }
-    }
 }
 
 pub(crate) fn parse_type(tokens: &mut TokenStream) -> Result<Option<DataType>> {
     if let Some((Token::Word(name), _)) = tokens.next() {
-        match name.as_str() {
-            "Int32" => Ok(Some(DataType::Int32)),
-            "Float32" => Ok(Some(DataType::Float32)),
-            "String" => Ok(Some(DataType::String)),
-            "Boolean" => Ok(Some(DataType::Boolean)),
-            "void" => Ok(None),
-            _ => Ok(Some(DataType::Custom(name))),
+        match DataType::from_str(name.as_str()) {
+            Ok(primitive) => Ok(Some(primitive)),
+            Err(_) => Ok(Some(DataType::Custom(name))),
         }
     } else {
         Err(anyhow!("Expected a type name"))
